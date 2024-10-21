a15174f175d67713189129f067c83f8a2fe40e37
angelMonzter <angelmonztr2000@hotmail.com>
2024-10-20 22:35:34 -0600

    Desplige app

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuración básica de Axios
 const axiosInstance = axios.create({
-    baseURL: 'http://softwareincorp.com.mx/:4000', // Cambia esto por la URL de tu API
+    baseURL: 'http://18.191.249.11:4000', // Cambia esto por la URL de tu API
     headers: {
         'Content-Type': 'application/json',
     },
