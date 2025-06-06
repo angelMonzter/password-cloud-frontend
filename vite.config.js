7408c017743967761bf22916ba91e4755a1245c3
angelMonzter <angelmonztr2000@hotmail.com>
2025-06-06 12:06:14 -0600

    Categorias listas

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/password_cloud/',
+  //base: '/password_cloud/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
